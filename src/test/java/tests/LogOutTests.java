cb24476ededf3d0b81e98b5b7fd5b129adf4553d
Tintina-1 <tintina.adv@yandex.com>
2025-01-20 02:36:15 +0300

    Added logout tests

---
+++ src/test/java/tests/LogOutTests.java
@@ -0,0 +1,73 @@
+package tests;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.LoginPage;
+import pages.MainPage;
+import pages.PersonalCabinetPage;
+import utils.ApiUtils;
+import utils.TestDataUtils;
+
+import java.time.Duration;
+
+public class LogOutTests extends BaseTest {
+
+    private ApiUtils apiUtils;
+    private String userEmail;
+    private String userPassword;
+    private String userName;
+    private String authToken;
+    private String expectedUrl;
+    private PersonalCabinetPage personalCabinetPage;
+
+    @Before
+    public void setUpTest() {
+        setUp("chrome");
+        driver.get(baseUrl);
+
+        apiUtils = new ApiUtils(baseUrl);
+        personalCabinetPage = new PersonalCabinetPage(driver);
+
+        userName = TestDataUtils.generateUniqueName();
+        userEmail = TestDataUtils.generateUniqueEmail();
+        userPassword = TestDataUtils.generateUniquePassword();
+
+        authToken = apiUtils.registerUser(userName, userEmail, userPassword);
+
+
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickPersonalCabinetButton();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(userEmail, userPassword);
+        mainPage.clickPersonalCabinetButton();
+
+        expectedUrl = "https://stellarburgers.nomoreparties.site/account/profile";
+    }
+
+
+    @Test
+    public void testLogOutFromProfile() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe(expectedUrl));
+
+        personalCabinetPage.clickLogOutButton();
+        wait.until(ExpectedConditions.urlToBe(baseUrl + "login"));
+
+        Assert.assertEquals("Редирект не на страницу логина", baseUrl + "login", driver.getCurrentUrl());
+    }
+
+
+    @After
+    public void tearDownTest() {
+        // Удаление пользователя через API после теста
+        if (authToken != null) {
+            apiUtils.deleteUser(authToken);
+        }
+        tearDown();
+    }
+}
